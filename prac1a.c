6aa4b1f22361e5e80fe13b56be4e38299991ab5c
S Prajwall Narayana <93190550+hacker69i@users.noreply.github.com>
2022-06-09 21:51:52 +0530

    Add files via upload

---
+++ prac1a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+printf("a)Have a Good day!\n");
+printf("b)have /\\a /\\good /\\day!\n");
+printf("c)laughter is the best \"medicine\".share the \"knowledge\".\n");
+printf("d) hello\n world!\n");
+printf("e) hello\v world!\n");
+printf("f)how are you ?\n");
+printf("f)I am fine \n");
+printf("g)how\tare\tyou\t?\n");
+printf("g)I\tam\tFine\n");
+printf("h)how\tare\tyou\t?\n I\tam\tfine\t.\t thank\tyou\t.\n");
+printf("j)bank interest is 9%% in the year 2018.\n");
+}
